b0dd36d860464c2cae2dd126d292eb7c540ac3ad
Thuận <thuannh@sunshinegroup.vn>
2022-12-21 17:57:50 +0700

    fix locator product detail name

--- pageFactory/objectRepository/ProductPageObjects.ts
+++ pageFactory/objectRepository/ProductPageObjects.ts
@@ -9,5 +9,5 @@ export class ProductPageObjects {
     protected static PRODUCT_CARD = `.pricetag .productcart`;
 
     protected static PRODUCT_FIL_NEPRICE = `.productfilneprice`;
-    protected static PRODUCT_DETAIL_NAME = `//h1[@class='productname']//span[1]`;
+    protected static PRODUCT_DETAIL_NAME = `.col-md-12 .productname`;
 }
